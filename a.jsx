912806ab44abd57b6c34c1eb44eaa2520800cfa3
WooYoung Cho <wy1.cho@samsung.com>
2018-01-19 15:36:27 +0900

    Update a.jsx

--- a.jsx
+++ a.jsx
@@ -6,6 +6,7 @@ class Hello extends React.Component {
             <div>
                 /* This is a comment */ {/* REACT_JSX_BAD_COMMENT alarm because this text is recognized as a JSX child instead of a comment. */}
                 <div>// is a double slash.</div> {/* REACT_JSX_BAD_COMMENT alarm because this text in div element is recognized as a JSX child instead of a comment. */}
+                <div>// is a double slash.</div> {/* REACT_JSX_BAD_COMMENT alarm because this text in div element is recognized as a JSX child instead of a comment. */}
             </div>
         );
     }
