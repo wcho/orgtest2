7d594eb64d54cba6606f055b2e5253f2d7a50b25
WooYoung Cho <wy1.cho@samsung.com>
2018-01-23 17:13:58 +0900

    Create tsfile.vue

---
+++ tsfile.vue
@@ -0,0 +1,39 @@
+<template>
+	<div>
+		<BaseInputText 
+			v-model="newTodoText"
+			placeholder="New todo"
+			@keydown.enter="addTodo"
+		/>
+		<ul v-if="todos.length">
+			<TodoListItem
+				v-for="todo in todos"
+				:key="todo.id"
+				:todo="todo"
+				@remove="removeTodo"
+			/>
+		</ul>
+		<p v-else>
+			Nothing left in the list. Add a new todo in the input above.
+		</p>
+	</div>
+</template>
+
+<script>
+import Vue from 'vue'
+x; // alarm
+declare module 'vue/types/vue' {
+  // Global properties can be declared
+  // on the `VueConstructor` interface
+  interface VueConstructor {
+    $myGlobal: string
+  }
+}
+
+// ComponentOptions is declared in types/options.d.ts
+declare module 'vue/types/options' {
+  interface ComponentOptions<V extends Vue> {
+    myOption?: string
+  }
+}
+</script>
